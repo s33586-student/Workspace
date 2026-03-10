ee5e3230c2437871d644f269a009b8e820da5b72
s33586-student <s33586@pjwstk.edu.pl>
2026-03-10 14:44:04 +0100

    Was implemented function CalculateAverage

--- Project.java
+++ Project.java
@@ -1,4 +1,12 @@
 class Project {
+    public static float CalculateAverage(int[] values) {
+        int sum = 0;
+        for (int i : values) {
+            sum += i;
+        }
+        return sum/values.length;
+    }
+
     public static void main(String[] args) {
         
     }
