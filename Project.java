28d5f9358aea82c54a13b0157b7967ea870f1f87
s33586-student <s33586@pjwstk.edu.pl>
2026-03-10 14:48:43 +0100

    Added to Main example of use CalculateAverage function

--- Project.java
+++ Project.java
@@ -8,6 +8,7 @@ class Project {
     }
 
     public static void main(String[] args) {
-        
+        int[] values = new int[10]{1,2,3,4,5,6,7,8,9,10};
+        System.out.println(CalculateAverage(values));
     }
 }
